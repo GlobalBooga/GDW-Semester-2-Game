6185037079ef2b2134cddc4d95bdaff80d2a542c
TylerReeds <117316771+TylerReeds@users.noreply.github.com>
2023-03-31 19:19:17 -0400

    menu stuff

--- Assets/Scripts/Menus/OptionsMenu.cs
+++ Assets/Scripts/Menus/OptionsMenu.cs
@@ -1,35 +1,65 @@
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+using Slider = UnityEngine.UI.Slider;
 
 public class OptionsMenu : MonoBehaviour
 {
-    [SerializeField]
-    private AudioMixer Master;
-    [SerializeField]
-    private AudioMixer SFX;
-    [SerializeField]
-    private AudioMixer Music;
-    [SerializeField]
-    private AudioSource AudioSource;
+    [Header("Volume")]
+    [SerializeField] private AudioMixer Master;
+    [SerializeField] private AudioMixer SFX;
+    [SerializeField] private AudioMixer Music;
+    [SerializeField] private AudioSource MasterSource;
+    [SerializeField] private AudioSource SFXSource;
+    [SerializeField] private AudioSource MusicSource;
 
+    [Header("Gameplay Settings")]
+    [SerializeField] private Slider controllerSensSlider;
+    public int mainControllerSen = 10;
+    [SerializeField] private Slider difficultySlider;
+    public int difficulty = 0; 
     private void Start()
     {
         Master.SetFloat("MasterVolume", Mathf.Log10(PlayerPrefs.GetFloat("MasterVolume", 1) * 20));
         SFX.SetFloat("SFXVolume", Mathf.Log10(PlayerPrefs.GetFloat("SFXVolume", 1) * 20));
         Music.SetFloat("MusicVolume", Mathf.Log10(PlayerPrefs.GetFloat("MusicVolume", 1) * 20));
     }
-    public void OnChangeSlider(float Value)
+    public void ChangeMasterVolume(float Value)
     {
         Master.SetFloat("MasterVolume", Mathf.Log10(Value) * 20);
-        SFX.SetFloat("SFXVolume", Mathf.Log10(Value) * 20);
-        Music.SetFloat("MusicVolume", Mathf.Log10(Value) * 20);
-
         PlayerPrefs.SetFloat("MasterVolume", Value);
         PlayerPrefs.Save();
+    }
+    public void ChangeSFXVolume(float Value)
+    {
+        SFX.SetFloat("SFXVolume", Mathf.Log10(Value) * 20);
         PlayerPrefs.SetFloat("SFXVolume", Value);
         PlayerPrefs.Save();
+    }
+    public void ChangeMusicVolume(float Value)
+    {
+        Music.SetFloat("MusicVolume", Mathf.Log10(Value) * 20);
         PlayerPrefs.SetFloat("MusicVolume", Value);
         PlayerPrefs.Save();
+    }
+
+    public void SetControllerSen(float sensitivity)
+    {
+        mainControllerSen = Mathf.RoundToInt(sensitivity);
+
+        Player p = LevelManager.instance.GetPlayer();
+        GameData gd = LevelManager.instance.LoadGameData();
+
+        // check if player is valid, if not, then there is no player - meaning we are in the main menu level.
+        if (p)
+        {
+            p.gamepadRotSpeed = mainControllerSen;
+        }
+
+        gd.gpRotSpeed = mainControllerSen;
+
+        LevelManager.instance.Save(gd);
     }
 }
   
--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -44,6 +44,7 @@ public class Player : MonoBehaviour
     public List<Slider> staminaBars;
     private SpriteRenderer sr;
     public GameObject flashlight;
+    private OptionsMenu optionsMenu;
 
     [Header("All Weapons"), Space(5f)]
     [SerializeField] private GameObject AdanasDualies;
@@ -115,6 +116,7 @@ public class Player : MonoBehaviour
         cc = gameObject.GetComponent<CircleCollider2D>();
         hpcomp = gameObject.GetComponent<HPComponent>();
         sr = GetComponent<SpriteRenderer>();
+        optionsMenu = GetComponent<OptionsMenu>();
 
         if (hpcomp)
         {
@@ -129,11 +131,13 @@ public class Player : MonoBehaviour
     {
         gameData = LevelManager.instance.LoadGameData();
 
+        gamepadRotSpeed = gameData.gpRotSpeed;
+
         originalPos = transform.position;
         originalRot = transform.rotation;
         rb.freezeRotation = true;
         rb.drag = accelerationDrag;
-        if (hpcomp)hpcomp.isInvincible = false;
+        if (hpcomp) hpcomp.isInvincible = false;
 
         if (weapon)
         {
@@ -144,7 +148,7 @@ public class Player : MonoBehaviour
     }
 
     private void Update()
-    {
+    { 
         // If we are still holding down attack button, continue attacking
         if (weapon)
         {
